46d8b212b7147b252cb6182ef33025c13dc93092
basilmnazar <basilmnazar@gmail.com>
2024-07-14 21:33:11 +0530

    updated

--- deal_app/urls.py
+++ deal_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
-      
+   path('deal_register',deal_register,name='deal_register'),  
     
 ]
--- deal_app/views.py
+++ deal_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def deal_register(request):
+    return render(request, 'deal_register.html')
